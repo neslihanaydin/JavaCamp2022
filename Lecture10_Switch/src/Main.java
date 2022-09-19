e908f0764c9d611f21d7bd117de72c48c6993dee
neslihanaydin <neslihanaydin43@gmail.com>
2022-09-19 11:57:38 -0700

    Java-Camp-2022-Lecture-10-Switch

---
+++ Lecture10_Switch/src/Main.java
@@ -0,0 +1,30 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		char grade = 'A';
+		
+		switch (grade) {
+		case 'A':
+			System.out.println("Mukemmel : Gectiniz");			
+			break;
+		case 'B':
+			System.out.println("Cok guzel : Gectiniz");			
+			break;
+		case 'C':
+			System.out.println("Iyi : Gectiniz");			
+			break;
+		case 'D':
+			System.out.println("Fena degil : Gectiniz");			
+			break;
+		case 'F':
+			System.out.println("Malesef kaldiniz");			
+			break;
+		
+		default:
+			System.out.println("Gecersiz not girdiniz");			
+		}
+
+	}
+
+}
