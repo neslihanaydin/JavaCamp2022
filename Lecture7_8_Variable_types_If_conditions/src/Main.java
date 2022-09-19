e4249b5f054a754a1ae005f495345ab90838fc95
neslihanaydin <neslihanaydin43@gmail.com>
2022-09-19 11:56:23 -0700

    Java-Camp-2022-Lecture-7-8-Variable-types-and-if-cond

---
+++ Lecture7_8_Variable_types_If_conditions/src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Lecture 7
+		double sayi = 12.5;
+		sayi = -129;
+		System.out.println(sayi);
+		char karakter = 'A';
+		boolean dogruMu = false;
+		
+		//Lecture 8
+		
+		int sayi2 = 24;
+		if(sayi2 < 20) {
+			System.out.println("Sayi 20'den kucuktur");
+		}else if(sayi2 == 20) {
+			System.out.println("Sayi 20'ye esittir");
+		}else {
+			System.out.println("Sayi 20'den buyuktur");
+		}
+
+	}
+
+}
