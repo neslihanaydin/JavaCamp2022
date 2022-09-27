bf48605e02adac0ebd48a83c92d4f23eb74b8f76
neslihanaydin <neslihanaydin43@gmail.com>
2022-09-26 20:20:17 -0700

    Java-Camp-2022-Lecture-30-Calculator-RecapDemo

---
+++ Lecture30_Calculator_RecapDemo/src/Calculate.java
@@ -0,0 +1,20 @@
+
+public class Calculate {
+	
+	public int sum(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	public int sub(int num1, int num2) {
+		return num1 - num2;
+	}
+	
+	public int multiply(int num1, int num2) {
+		return num1 * num2;
+	}
+	
+	public int divide(int num1, int num2) {
+		return num1 / num2;
+	}
+
+}
---
+++ Lecture30_Calculator_RecapDemo/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Calculate calculate = new Calculate();
+		int result = calculate.sum(3, 4);
+		System.out.println(result);
+	}
+
+}
